af571d57244b073fd3a7beae68ab378ab00bb335
Mahmoud Taleb <32706209+mahmoudTaleb@users.noreply.github.com>
2018-04-20 10:37:04 +0200

    district class created

---
+++ src/main/java/com/capgemini/District.java
@@ -0,0 +1,17 @@
+package com.capgemini;
+
+public class District {
+    private int districtNo;
+
+    public District(int districtNo) {
+        this.districtNo = districtNo;
+    }
+
+    public int getDistrictNo() {
+        return districtNo;
+    }
+
+    public void setDistrictNo(int districtNo) {
+        this.districtNo = districtNo;
+    }
+}
